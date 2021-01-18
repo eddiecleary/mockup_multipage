9176d9d9ce4c3b99fc727f74c53d7ed4f4913974
Eddie Cleary <eddie@eddiecleary.com>
2021-01-18 12:42:55 -0500

    add media query to scoltrigger gsap, max width to card paragraph

--- js/index.js
+++ js/index.js
@@ -1,16 +1,22 @@
 // GSAP
 gsap.registerPlugin(ScrollTrigger);
 
-let sections = gsap.utils.toArray(".card");
+ScrollTrigger.matchMedia({
+    "(min-width: 992px)": function() {
 
-gsap.to(sections, {
-    xPercent: -100 * (sections.length - 1),
-    ease: "none",
-    scrollTrigger: {
-        trigger: ".cards",
-        pin: true,
-        scrub: 1,
-        snap: 1 / (sections.length - 1),
-        end: () => "+=" + (document.querySelector(".cards").offsetWidth / 2)
+        let sections = gsap.utils.toArray(".card");
+
+        gsap.to(sections, {
+            xPercent: -100 * (sections.length - 1),
+            ease: "none",
+            scrollTrigger: {
+                trigger: ".cards",
+                pin: true,
+                scrub: 1,
+                snap: 1 / (sections.length - 1),
+                // end: () => "+=" + (document.querySelector(".cards").offsetWidth / 2)
+            }
+        });
     }
 });
+
